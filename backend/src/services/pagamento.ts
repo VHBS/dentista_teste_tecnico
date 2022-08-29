ff2d3d0a5ce3b18924dfd29f6db93599b3b50298
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-29 17:57:50 -0300

    feat: service para cadastrar pagamento criado e implementado

--- backend/src/services/pagamento.ts
+++ backend/src/services/pagamento.ts
@@ -1,23 +1,9 @@
-// import Pagamento from './sequelize/models/Pagamento';
+import TypePagamento from '../@types/pagamento';
+import Pagamento from '../sequelize/models/Pagamento';
 
-// try {
-// const result = await Pagamento.findAll();
+const servicePagamentoCreate = async ({ data, valor }: TypePagamento) => {
+  const result = await Pagamento.create({ data, valor });
+  return result;
+};
 
-//   console.log(result);
-// } catch (error) {
-//   console.log(error);
-// }
-// const result = await Pagamento.create({ valor: 700, data: '2022-08-01' });
-// const result = await Pagamento.findAll({
-//   where: {
-//     data: {
-//       $between: ['2022-08-01', '2022-08-03'],
-//     },
-//   },
-// });
-
-// toLocaleDateString('pt-Br', {
-//   day: '2-digit',
-//   month: '2-digit',
-//   year: 'numeric',
-// })
+export default servicePagamentoCreate;
