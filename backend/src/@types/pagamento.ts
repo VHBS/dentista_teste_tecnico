22e45fe8493cd456d8c097f81a425695663105f2
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-31 18:56:56 -0300

    fix: atualizando tipagem do pagamento

--- backend/src/@types/pagamento.ts
+++ backend/src/@types/pagamento.ts
@@ -1,6 +1,22 @@
-type TypePagamento = {
+type TypePagamentoSaida = {
+  id: number;
   data: string;
-  valor: string;
+  valor: number;
+  parcela: number;
+  totalDeParcelas: number;
 };
 
+type TypePagamentoEntrada = {
+  data: string;
+  valor: string;
+  parcelas: string;
+};
+
+type TypeMensagemDeErro = {
+  menssagem: string;
+};
+
+type TypePagamento = TypePagamentoEntrada | TypePagamentoSaida;
+
 export default TypePagamento;
+export { TypeMensagemDeErro, TypePagamentoEntrada, TypePagamentoSaida };
