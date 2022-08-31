06bc9d64d49f3288b1f78e556016ed0b564c14ca
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-31 18:58:54 -0300

    fix: atualizando middleware de erro global

--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -1,15 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { TypeMensagemDeErro } from '../@types/pagamento';
 
 const errorMiddleware = async (
   err: ErrorRequestHandler,
   _req: Request,
   res: Response,
   _next: NextFunction,
-): Promise<Response> => {
+): Promise<Response<TypeMensagemDeErro>> => {
   console.log(err);
-  return res.status(500).json({ message: 'Erro inesperado' });
+  return res.status(500).json({ menssage: 'Erro inesperado' });
 };
 
 export default errorMiddleware;
