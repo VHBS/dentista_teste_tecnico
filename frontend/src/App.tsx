a111ebac0293a193f9ea5fb5616f89bfc9ee84f4
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-01 16:44:59 -0300

    feat: barra de navegacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './styles/index.css';
 
+import BarraDeNavegacao from './components/BarraDeNavegacao';
 import CadastrarPagamento from './pages/CadastrarPagamento';
 import Inicio from './pages/Inicio';
 
 function App() {
   return (
     <BrowserRouter>
+      <BarraDeNavegacao />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/pagamento" element={<CadastrarPagamento />} />
