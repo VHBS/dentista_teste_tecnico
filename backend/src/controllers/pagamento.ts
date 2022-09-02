f6410b2ceb7eed80efe40fed365efa4fc22f7bea
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-02 12:57:06 -0300

    feat: controller filtrar pagamento por data

--- backend/src/controllers/pagamento.ts
+++ backend/src/controllers/pagamento.ts
@@ -1,19 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
-import TypePagamento from '../@types/pagamento';
-import servicePagamentoCreate from '../services/pagamento';
+import { TypePagamentoSaida } from '../@types/pagamento';
+import { serviceCriarPagamento, serviceFiltrarPagamentoPorData } from '../services/pagamento';
 
-const controllerPagamentoCreate = async (
+const controllerCriarPagamento = async (
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<Response<TypePagamento> | void> => {
+): Promise<Response<TypePagamentoSaida[]> | void> => {
   try {
     const { data, valor, parcelas } = req.body;
-    const result = await servicePagamentoCreate({ data, valor, parcelas });
+    const result = await serviceCriarPagamento({ data, valor, parcelas });
     return res.status(200).json(result);
   } catch (error) {
     return next(error);
   }
 };
 
-export default controllerPagamentoCreate;
+const controllerFiltrarPagamentoPorData = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response<TypePagamentoSaida> | void> => {
+  try {
+    const { dataInicial, dataFinal } = req.body;
+    const result = await serviceFiltrarPagamentoPorData({ dataInicial, dataFinal });
+    return res.status(result.status).json(result.resposta);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export { controllerCriarPagamento, controllerFiltrarPagamentoPorData };
