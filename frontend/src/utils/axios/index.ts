4afbc03db6da8ce089c6ce5c71162bc3020953d7
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-02 18:24:46 -0300

    feat: requisicao para filtrar os pagamentos pela data

--- frontend/src/utils/axios/index.ts
+++ frontend/src/utils/axios/index.ts
@@ -1,22 +1,33 @@
 import axios from 'axios';
 
-import TypePagamentoEntrada from '../../@types/pagamento';
+import TypePagamentoEntrada, {
+  TypeFiltroPorDatas,
+  TypePagamentoCadastrado,
+} from '../../@types/pagamento';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001',
 });
 
-const testandoAxios = async () => {
-  const { data } = await api.get('/teste');
-
-  return data;
-};
-
-const requisicaoCriarPagamento = async (body: TypePagamentoEntrada) => {
+const requisicaoCriarPagamento = async (
+  body: TypePagamentoEntrada
+): Promise<TypePagamentoCadastrado[]> => {
   const { data } = await api.post('/pagamento', body);
 
   return data;
 };
 
-export { testandoAxios, requisicaoCriarPagamento };
+const requisicaoFiltrarPagamentosPorData = async (
+  body: TypeFiltroPorDatas
+): Promise<TypePagamentoCadastrado[]> => {
+  const params = new URLSearchParams();
+  params.append('dataInicial', body.dataInicial);
+  params.append('dataFinal', body.dataFinal);
+
+  const { data } = await api.get('/filtrar-pagamento', { params });
+
+  return data;
+};
+
+export { requisicaoCriarPagamento, requisicaoFiltrarPagamentosPorData };
 export default api;
