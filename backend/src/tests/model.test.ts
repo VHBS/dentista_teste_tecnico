14f5834df71d6e388edbaf93192538858920ce8e
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-04 01:53:55 -0300

    test: model de pagamento testado

---
+++ backend/src/tests/model.test.ts
@@ -0,0 +1,21 @@
+import { modelPagamento } from '../factory/FactoryPagamento';
+import Pagamento from '../sequelize/models/Pagamento';
+import {
+  mockFiltrarPagamentoEntrada,
+  mockPagamentosCriados,
+  mockPagamentosFiltradosPorData,
+} from './mocks/pagamento/mocksPagamentos';
+
+describe('Testando a camada Model de Pagamento', () => {
+  it('Pagamento cadastrado com sucesso', async () => {
+    jest.spyOn(Pagamento, 'bulkCreate').mockResolvedValue(mockPagamentosCriados as Pagamento[]);
+    const pagamentosCadastrados = await modelPagamento.criarPagamento(mockPagamentosCriados);
+    expect(pagamentosCadastrados).toBe(mockPagamentosCriados);
+  });
+
+  it('Filtrando pagamentos por data', async () => {
+    jest.spyOn(Pagamento, 'findAll').mockResolvedValue(mockPagamentosFiltradosPorData as Pagamento[]);
+    const pagamentosCadastrados = await modelPagamento.filtrarPagamentoPorData(mockFiltrarPagamentoEntrada);
+    expect(pagamentosCadastrados).toBe(mockPagamentosFiltradosPorData);
+  });
+});
