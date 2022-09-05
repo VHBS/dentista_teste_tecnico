1be9e7c59cf3decd5e238362ea9e5586e1767658
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-05 00:00:10 -0300

    style: adicionando estilo global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './styles/index.css';
+import { createGlobalStyle } from 'styled-components';
 
 import BarraDeNavegacao from './components/BarraDeNavegacao';
 import CadastrarPagamento from './pages/CadastrarPagamento';
 import FiltrarPagamentoPorData from './pages/FiltrarPagamentoPorData';
 import Inicio from './pages/Inicio';
 
+const EstiloGlobal = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap');
+  * {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: 'Fira Sans', sans-serif; 
+  }
+
+  body {
+  width: 100vw;
+  background-color: #cacaca;
+}
+`;
+
 function App() {
   return (
     <BrowserRouter>
+      <EstiloGlobal />
       <BarraDeNavegacao />
       <Routes>
         <Route path="/" element={<Inicio />} />
