555f1f62f8c6d40e88a1f6f3a6e081734d615ba8
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-09-05 00:01:18 -0300

    style: estilizando barra de navegacao

--- frontend/src/components/BarraDeNavegacao.tsx
+++ frontend/src/components/BarraDeNavegacao.tsx
@@ -1,18 +1,24 @@
 import { useNavigate } from 'react-router-dom';
 
+import { Button } from '../styles/componentesGenericos';
+import { Nav } from '../styles/Nav';
+
 export default function BarraDeNavegacao() {
   const navigate = useNavigate();
   return (
-    <nav>
-      <button type="button" onClick={() => navigate('/')}>
-        Inicio
-      </button>
-      <button type="button" onClick={() => navigate('/pagamento')}>
-        Cadastrar Pagamento
-      </button>
-      <button type="button" onClick={() => navigate('/filtrar-pagamento')}>
-        Filtrar Pagamentos
-      </button>
-    </nav>
+    <Nav>
+      <h3>Doutora Érica</h3>
+      <div className="container">
+        <Button type="button" onClick={() => navigate('/')}>
+          Inicio
+        </Button>
+        <Button type="button" onClick={() => navigate('/pagamento')}>
+          Cadastrar Pagamento
+        </Button>
+        <Button type="button" onClick={() => navigate('/filtrar-pagamento')}>
+          Filtrar Pagamentos
+        </Button>
+      </div>
+    </Nav>
   );
 }
---
+++ frontend/src/styles/Nav.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  @media (max-width: 380px) {
+    height: 6em;
+    h3 {
+      display: none;
+    }
+    .container {
+      height: 6em;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+    }
+  }
+  @media (min-width: 380px) and (max-width: 532px) {
+    height: 4em;
+  }
+  height: 2em;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgba(155, 155, 155, 0.7);
+
+  h3 {
+    padding: 0 0.5em;
+    text-align: center;
+  }
+
+  .container {
+    padding: 0 0.5em;
+    display: flex;
+  }
+  position: fixed;
+`;
